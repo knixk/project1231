e91cf9b690998b4db65038b2b057d5ae8a35dfa3
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-09-14 19:03:44 +0530

    changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import { PrimeReactProvider } from "primereact/api";
 import { ChatList } from "react-chat-elements";
 import { MessageList } from "react-chat-elements";
 import React from "react";
+
 const messageListReferance = React.createRef();
 import { chat } from "./data";
 
@@ -12,7 +13,28 @@ export default function App() {
         <div className="left">
           <ChatList className="chat-list" dataSource={chat} />
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <MessageList
+            referance={messageListReferance}  
+            className="message-list"
+            lockable={true}
+            toBottomHeight={"100%"}
+            dataSource={[
+              {
+                position: "left",
+                type: "text",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+                date: new Date(),
+              },
+              {
+                position: "right",
+                type: "text",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+                date: new Date(),
+              },
+            ]}
+          />
+        </div>
       </div>
     </PrimeReactProvider>
   );
